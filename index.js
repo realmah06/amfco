8d320ef9900157c443718ab0559b630a528b7b25
realmah06 <154765144+realmah06@users.noreply.github.com>
2024-11-17 01:31:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function login() {
     var success_alert = document.getElementById("successAlert");
     var error_alert = document.getElementById("errorAlert");
     var loader = document.getElementById("loader");
-    var username = document.getElementById("username");
-    var password = document.getElementById("password");
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
     // Show loader and hide other elements initially
     loader.style.display = 'flex';
@@ -12,20 +12,18 @@ function login() {
     success_alert.style.display = 'none';
     error_alert.style.display = 'none';
 
+    // Delay for 5 seconds before showing the alert
+    setTimeout(() => {
+        loader.style.display = 'none'; // Hide the loader
+        overlay.style.display = 'block';
 
-   
-        // Delay for 5 seconds before showing the alert
-        setTimeout(() => {
-            loader.style.display = 'none'; // Hide the loader
-            overlay.style.display = 'block';
-
-            if (username === 'admin' && password === 'admin') {
-                success_alert.style.display = 'block';
-            } else {
-                error_alert.style.display = 'block';
-            }
-        }, 5000); // 5000 ms = 5 seconds
-    }
+        if (username === 'admin' && password === 'admin') {
+            success_alert.style.display = 'block';
+        } else {
+            error_alert.style.display = 'block';
+        }
+    }, 5000); // 5000 ms = 5 seconds
+}
 
 function closePopup() {
     document.getElementById("overlay").style.display = 'none';
