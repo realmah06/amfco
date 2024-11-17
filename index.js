af12cc74509f24519fb780cc84b959e18e4478d6
realmah06 <154765144+realmah06@users.noreply.github.com>
2024-11-17 01:28:38 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+function login() {
+    var overlay = document.getElementById("overlay");
+    var success_alert = document.getElementById("successAlert");
+    var error_alert = document.getElementById("errorAlert");
+    var loader = document.getElementById("loader");
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+
+    // Show loader and hide other elements initially
+    loader.style.display = 'flex';
+    overlay.style.display = 'none';
+    success_alert.style.display = 'none';
+    error_alert.style.display = 'none';
+
+
+   
+        // Delay for 5 seconds before showing the alert
+        setTimeout(() => {
+            loader.style.display = 'none'; // Hide the loader
+            overlay.style.display = 'block';
+
+            if (username === 'admin' && password === 'admin') {
+                success_alert.style.display = 'block';
+            } else {
+                error_alert.style.display = 'block';
+            }
+        }, 5000); // 5000 ms = 5 seconds
+    }
+
+function closePopup() {
+    document.getElementById("overlay").style.display = 'none';
+    document.getElementById("successAlert").style.display = 'none';
+    document.getElementById("errorAlert").style.display = 'none';
+}
